a62e3507853204576e6e2e22c6466ff93a20d99a
itsNeer <Deepanshukhorwal7@gmail.com>
2024-10-13 00:13:19 +0530

    project

---
+++ script.js
@@ -0,0 +1,42 @@
+let arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  
+  function PrintDeveloperbyMap() {
+    arr.map(employee => {
+      if (employee.profession === "developer") {
+        console.log(employee);
+      }
+    });
+  }
+
+  function PrintDeveloperbyForEach() {
+    arr.forEach(employee => {
+      if (employee.profession === "developer") {
+        console.log(employee);
+      }
+    });
+  }
+
+  function addData() {
+    let employee = { id: 4, name: "susan", age: "20", profession: "intern" };
+    arr.push(employee);
+    console.log(arr);
+  }
+
+  function removeAdmin() {
+    let updatedArr = arr.filter(employee => employee.profession !== "admin");
+    console.log(updatedArr);
+  }
+
+  function concatenateArray() {
+    let newArr = [
+      { id: 5, name: "Nikhil", age: "21", profession: "developer" },
+      { id: 6, name: "Neeraj", age: "22", profession: "developer" },
+      { id: 7, name: "Himanshu", age: "23", profession: "developer" }
+    ];
+    let concatenatedArr = arr.concat(newArr);
+    console.log(concatenatedArr);
+  }
